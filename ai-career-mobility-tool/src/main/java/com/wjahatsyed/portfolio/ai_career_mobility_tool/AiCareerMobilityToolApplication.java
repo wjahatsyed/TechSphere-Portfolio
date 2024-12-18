75582a24f3d3b80275e2dac039d254c89446b896
Wajahat Adil <wajahatsyed1257@gmail.com>
2024-12-18 22:53:16 +0500

    Added basic project for AI Career Mobility Tool.

---
+++ ai-career-mobility-tool/src/main/java/com/wjahatsyed/portfolio/ai_career_mobility_tool/AiCareerMobilityToolApplication.java
@@ -0,0 +1,13 @@
+package com.wjahatsyed.portfolio.ai_career_mobility_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCareerMobilityToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCareerMobilityToolApplication.class, args);
+	}
+
+}
---
+++ ai-career-mobility-tool/src/test/java/com/wjahatsyed/portfolio/ai_career_mobility_tool/AiCareerMobilityToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjahatsyed.portfolio.ai_career_mobility_tool;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCareerMobilityToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
